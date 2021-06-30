59b814de0625808d8a771c1a067b89d603d3df4f
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-06-30 18:03:16 -0400

    stuff

--- Debug/neko/haxe/ApplicationMain.hx
+++ Debug/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "8";
+		app.meta["build"] = "10";
 		app.meta["company"] = "Keystone Games";
 		app.meta["file"] = "SoundSpace";
 		app.meta["name"] = "SoundSpace";
--- build.bat
+++ build.bat
@@ -4,11 +4,17 @@ if %1 == test (
     lime test neko -debug
 )
 
-if %1 == release (
-    lime build windows
-    echo %2>Release\windows\bin\version.txt
-    lime build mac
-    echo %2>Release\neko\bin\SoundSpace.app\version.txt
-    lime build linux
-    echo %2>Release\neko\bin\version.txt
+if %1 == create (
+    if %2 == release (
+        lime build windows
+        echo %3>Release\windows\bin\version.txt
+        lime build mac
+        echo %3>Release\neko\bin\SoundSpace.app\version.txt
+        lime build linux
+        echo %3>Release\neko\bin\version.txt
+    )
+    if %2 == browsergame (
+        lime build html5
+        echo %3>Release\html5\bin\version.txt
+    )
 )
