dda5935c79a169b27302c7d8c718c06df70c8361
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-09-29 14:23:28 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Building for windows
 lime build windows -release
-echo %3>Release\windows\bin\version.txt
+echo %1>Release\windows\bin\version.txt
 echo Building for macOS
 lime build mac -release
-echo %3>Release\neko\bin\SoundSpace.app\version.txt
+echo %1>Release\neko\bin\SoundSpace.app\version.txt
 echo Building for linux
 lime build linux -release
-echo %3>Release\neko\bin\version.txt
+echo %1>Release\neko\bin\version.txt
 echo Building for html5
 lime build html5 -release
-echo %3>Release\html5\bin\version.txt
+echo %1>Release\html5\bin\version.txt
