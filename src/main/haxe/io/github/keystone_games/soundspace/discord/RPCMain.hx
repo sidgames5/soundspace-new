8e17bc0e98fd843c53f03b6f06f4879bf3871888
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-09-29 08:20:37 -0400

    Update RPCMain.hx

--- src/main/haxe/io/github/keystone_games/soundspace/discord/RPCMain.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/discord/RPCMain.hx
@@ -1,9 +1,11 @@
 package io.github.keystone_games.soundspace.discord;
 
+using io.github.keystone_games.soundspace.discord.DiscordRPC;
+
 class RPCMain {
-    public static function init() {
-        
-    }
+    public static function init() {}
     
-    public static function update(_options : DiscordPresenceOptions
+    public static function update(_options : DiscordPresenceOptions) { DiscordRPC.presence(_options); }
+    
+    public static function shutdown() { DiscordRPC.shutdown(); }
 }
