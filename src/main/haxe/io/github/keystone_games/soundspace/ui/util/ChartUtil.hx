6b4d45476c27329a0a5ba351b75c686aa06a2110
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-10-03 19:57:29 -0400

    Update ChartUtil.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/util/ChartUtil.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/util/ChartUtil.hx
@@ -1,5 +1,8 @@
 package io.github.keystone_games.soundspace.ui.util;
 
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+import flixel.FlxState;
 import haxe.Json;
 import haxe.io.Path;
 #if sys
@@ -23,9 +26,31 @@ class ChartParser
 
 class ChartUtil
 {
-	public static function createMap(sInfo:{name:String, artist:String, album:String}, file:Path) { }
+	public static function createMap(sInfo:{name:String, artist:String, album:String}, file:Path) {}
 }
 
-class Timeline extends FlxState {
-	public override function create() { super.create(); }
+class Timeline extends FlxState
+{
+	public static var c_xres:Int;
+	public static var c_yres:Int;
+	public static var c_color:FlxColor;
+	public static var c_lanes:Int;
+
+	var box:FlxSprite;
+
+	public override function create()
+	{
+		super.create();
+
+		box = new FlxSprite();
+		box.makeGraphic(c_xres, c_yres, c_color);
+		box.screenCenter(X);
+	}
+
+	public static function configure(xres:Int = 3840, yres:Int = 1680, color:FlxColor, lanes:Int = 4) {
+		c_xres = xres;
+		c_yres = yres;
+		c_color = color;
+		c_lanes = lanes;
+	}
 }
