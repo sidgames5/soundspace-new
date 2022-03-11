3ea1a94e132519d5852c7cd66e740bb9ce787c4f
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-11 09:58:49 -0500

    Delete updater.bat

--- resources/data/updater.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set dfgkjggdjlgguegsdjkghfuirdhg=version.txt
-echo Current SoundSpace version: %more version.txt%
-set old=more version.txt
-echo Detecting latest version...
-powershell -Command Invoke-WebRequest nul dfgkjggdjlgguegsdjkghfuirdhg > nul
-echo Latest version: %more version.txt%
