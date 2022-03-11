c70bc48e48b05e1ca7f23098466143919f2a13f9
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-11 13:01:56 -0500

    Update StateLoading.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
@@ -3,10 +3,22 @@ package io.github.keystone_games.soundspace.ui.scene;
 import flixel.FlxState;
 
 class StateLoading extends FlxState {
+  public static var bar:FlxSprite;
+  
   public override function create() { 
     super.create();
+    
+    bar = new FlxSprite();
+    bar.makeGraphic(64, 64);
+    bar.screenCenter(Y);
+    bar.x = 0;
+    add(bar);
   }
   public override function update(dt:Float) { 
     super.update(dt);
+    
+    if (bar.x < 3840) {
+      bar.x += 1;
+    } else { bar.x -= 1; }
   }
 }
