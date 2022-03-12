c162fa484cdc514251ea373c62c4f3f591eb67d6
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-12 13:16:03 -0500

    Update StateLoading.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
@@ -1,24 +1,34 @@
 package io.github.keystone_games.soundspace.ui.scene;
 
 import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
 
 class StateLoading extends FlxState {
-  public static var bar:FlxSprite;
-  
-  public override function create() { 
-    super.create();
-    
-    bar = new FlxSprite();
-    bar.makeGraphic(64, 64);
-    bar.screenCenter(Y);
-    bar.x = 0;
-    add(bar);
-  }
-  public override function update(dt:Float) { 
-    super.update(dt);
-    
-    if (bar.x < 3840) {
-      bar.x += 1;
-    } else { bar.x -= 1; }
-  }
+	public static var bar:FlxSprite;
+  public static var bgLeft:Bool;
+
+	public override function create() {
+		super.create();
+
+		bar = new FlxSprite();
+		bar.makeGraphic(64, 64);
+		bar.screenCenter(Y);
+		bar.x = 0;
+		add(bar);
+	}
+
+	public override function update(dt:Float) {
+		super.update(dt);
+
+    switch (bgLeft) {
+      case true:
+        bar.x -= 1;
+      case false:
+        bar.x += 1;
+    }
+    if (bar.x > FlxG.width) bgLeft = true;
+    if (bar.x < 0) bgLeft = false;
+    trace(bar.x);
+	}
 }
