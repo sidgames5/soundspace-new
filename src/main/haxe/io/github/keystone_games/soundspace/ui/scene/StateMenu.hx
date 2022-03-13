6b80f72a43c7599b4461dca508c3bbe017d66130
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-13 09:34:49 -0400

    Update StateMenu.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateMenu.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateMenu.hx
@@ -2,4 +2,11 @@ package io.github.keystone_games.soundspace.ui.scene;
 
 import flixel.FlxState;
 
-class StateMenu extends FlxState {}
+class StateMenu extends FlxState {
+    public override function create() {
+        super.create();
+    }
+    public override function update(dt:Float) {
+        super.update(dt);
+    }
+}
