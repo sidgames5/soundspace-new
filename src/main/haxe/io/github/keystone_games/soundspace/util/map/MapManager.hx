37fac29eba9f89b09cbafc6830e20a2cae01182b
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-14 10:13:20 -0400

    Update MapManager.hx

--- src/main/haxe/io/github/keystone_games/soundspace/util/map/MapManager.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/util/map/MapManager.hx
@@ -2,6 +2,9 @@ package io.github.keystone_games.soundspace.util.map;
 
 class MapManager {
   public static function download(id:Int) {
+    #if debug
+      Logger.debug("Checking for sys package");
+    #end
     #if sys
     #else
       Logger.warn("Unable to download map " + id + ": sys package unavailable");
