c88081a3754083de77f5ab32b9a5fb054f6e52fd
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-14 08:14:38 -0400

    Update StateMapSelector.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateMapSelector.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateMapSelector.hx
@@ -4,8 +4,18 @@ import io.github.keystone_games.soundspace.lists.Modes;
 import flixel.FlxState;
 
 class StateMapSelector extends FlxState {
+    public static var mapList:Modes;
+    
     public function new(list:Modes) {
         super();
+        
+        /*switch (list) {
+            case CLASSIC:
+            case: PONG:
+            case: KEYBOARD:
+            case: BEAT:
+        }*/
+        mapList = list;
     }
     public override function create() {
         super.create();
