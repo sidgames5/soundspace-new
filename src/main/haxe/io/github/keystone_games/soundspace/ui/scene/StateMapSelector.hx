06aaf01e777cc5cf8d20bf50183bf226438b0213
BasicCorruption <saytleader@outlook.com>
2022-03-14 07:29:24 -0400

    Add StateMapSelector.hx

---
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateMapSelector.hx
@@ -0,0 +1,16 @@
+package io.github.keystone_games.soundspace.ui.scene;
+
+import io.github.keystone_games.soundspace.lists.Modes;
+import flixel.FlxState;
+
+class StateMapSelector extends FlxState {
+    public function new(list:Modes) {
+        super();
+    }
+    public override function create() {
+        super.create();
+    }
+    public override function update(dt:Float) {
+        super.update(dt);
+    }
+}
