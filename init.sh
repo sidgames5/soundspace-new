838c5277acc65f8d41ef167feb7e4e4413ffd212
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-17 14:39:16 -0400

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+apt install haxe
+haxelib setup
+haxelib install openfl
+haxelib install lime
+haxelib install flixel
+haxelib install flixel-tools
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib run flixel-tools setup
