db12f5c72e5c4720788affbd295fd43331fb9fd1
BasicCorruption <saytleader@outlook.com>
2022-03-21 17:42:53 -0400

    remove useless stuff

--- src/main/haxe/io/github/keystone_games/soundspace/Reference.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/Reference.hx
@@ -1,15 +1,9 @@
 package io.github.keystone_games.soundspace;
 
-import lime.app.Application;
-
-class Reference
-{
+class Reference {
 	public static final APP_NAME:String = "SoundSpace";
 	public static final APP_ID:String = "soundspace";
-	public static final VERSION = "0.0.3+dev.167";
+	public static final VERSION = "0.0.3+dev.171";
 
 	public static var VOLUME_MULTIPLIER:Float = 1;
-	public static var ASSET_GROUP:String = "Default";
-
-	public static var LAST_MAP:String;
 }
