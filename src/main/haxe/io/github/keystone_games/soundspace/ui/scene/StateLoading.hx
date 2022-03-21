5df28079db95bbf3e1a43bbc6762144da9a947ea
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-20 21:08:37 -0400

    Update StateLoading.hx

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/StateLoading.hx
@@ -1,24 +1,14 @@
 package io.github.keystone_games.soundspace.ui.scene;
 
-import io.github.keystone_games.kglog.Logger;
-import flixel.util.FlxColor;
-import flixel.text.FlxText;
 import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.FlxG;
 
-class StateLoading extends FlxState
-{
+class StateLoading extends FlxState {
 	public static var bar:FlxSprite;
-	public static var bgLeft:Bool;
-	public static var titleText:FlxText;
-	public static var lpc:Int = 1;
-	public static var lpcTimer:Float;
+  public static var bgLeft:Bool;
 
-	public static var loaded:Bool = false;
-
-	public override function create()
-	{
+	public override function create() {
 		super.create();
 
 		bar = new FlxSprite();
@@ -26,60 +16,19 @@ class StateLoading extends FlxState
 		bar.screenCenter(Y);
 		bar.x = 0;
 		add(bar);
-
-		titleText = new FlxText(0, 0, 0, "Loading.").setFormat(null, 32, FlxColor.WHITE, CENTER);
-		titleText.screenCenter(X);
-		titleText.y = FlxG.height / 8;
-		add(titleText);
-
-		FlxG.drawFramerate = 15;
-		FlxG.updateFramerate = 60;
-
-		loaded = true;
 	}
 
-	public override function update(dt:Float)
-	{
+	public override function update(dt:Float) {
 		super.update(dt);
 
-		if (loaded)
-			FlxG.switchState(new StateMenu());
-
-		switch (bgLeft)
-		{
-			case true:
-				bar.x -= 5;
-			case false:
-				bar.x += 5;
-		}
-		if (bar.x > FlxG.width - bar.width)
-			bgLeft = true;
-		if (bar.x < 0)
-			bgLeft = false;
-		Logger.debug(bar.x);
-
-		lpcTimer += dt;
-		Logger.debug(lpcTimer);
-		if (lpcTimer >= (1 / 3))
-		{
-			if (lpc == 3)
-			{
-				lpc = 1;
-			}
-			else
-			{
-				lpc++;
-			}
-			lpcTimer = 0;
-			switch (lpc)
-			{
-				case 1:
-					titleText.text = "Loading.";
-				case 2:
-					titleText.text = "Loading..";
-				case 3:
-					titleText.text = "Loading...";
-			}
-		}
+    switch (bgLeft) {
+      case true:
+        bar.x -= 1;
+      case false:
+        bar.x += 1;
+    }
+    if (bar.x > FlxG.width) bgLeft = true;
+    if (bar.x < 0) bgLeft = false;
+    trace(bar.x);
 	}
 }
