75e9eee07c80ccaba9e334f772b8e3a0b09706a8
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-26 17:55:34 -0400

    update version

--- src/main/haxe/io/github/keystone_games/soundspace/Reference.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/Reference.hx
@@ -3,7 +3,7 @@ package io.github.keystone_games.soundspace;
 class Reference {
 	public static final APP_NAME:String = "SoundSpace";
 	public static final APP_ID:String = "soundspace";
-	public static final VERSION = "0.0.4+dev.177";
+	public static final VERSION = "0.0.4+dev.179";
 
 	public static var VOLUME_MULTIPLIER:Float = 1;
 }
