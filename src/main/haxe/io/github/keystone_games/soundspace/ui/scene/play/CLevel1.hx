4c1f27610dcbb2c6a7130cb4688d813b11944ca8
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-03-25 22:15:33 -0400

    Add lanes

--- src/main/haxe/io/github/keystone_games/soundspace/ui/scene/play/CLevel1.hx
+++ src/main/haxe/io/github/keystone_games/soundspace/ui/scene/play/CLevel1.hx
@@ -1,5 +1,6 @@
 package io.github.keystone_games.soundspace.ui.scene.play;
 
+import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.text.FlxText;
@@ -11,7 +12,14 @@ class CLevel1 extends FlxState {
 	public static var time:Float = 0;
 	public static var tempTime:Float = 0;
 	public static var beats:Int = 0;
-	public static var bpm:Float = 127;
+	public static final bpm:Float = 127;
+
+	public static var lane1:FlxSprite;
+	public static var lane2:FlxSprite;
+	public static var lane3:FlxSprite;
+	public static var lane4:FlxSprite;
+
+	public static var note1:FlxSprite;
 
 	public override function create() {
 		super.create();
@@ -21,6 +29,33 @@ class CLevel1 extends FlxState {
 		levelIndicator = new FlxText(0, 0, 0, "Level 1").setFormat("resources/assets/fonts/monsterrat.ttf", 32, FlxColor.BLUE, CENTER);
 		levelIndicator.screenCenter(X);
 		add(levelIndicator);
+
+		lane1 = new FlxSprite();
+		lane1.makeGraphic(Std.int(FlxG.width / 8), Std.int((FlxG.height / 16) * 12), FlxColor.fromString("#ff3e3e3e"));
+		lane1.screenCenter(X);
+		lane1.y = Std.int(FlxG.height / 16);
+		lane1.x -= Std.int(lane1.width / 2) * 2;
+		add(lane1);
+		lane2 = new FlxSprite();
+		lane2.makeGraphic(Std.int(FlxG.width / 8), Std.int((FlxG.height / 16) * 12), FlxColor.fromString("#ff3e3e3e"));
+		lane2.screenCenter(X);
+		lane2.y = Std.int(FlxG.height / 16);
+		lane2.x -= Std.int(lane2.width / 2) * 1;
+		add(lane2);
+		lane3 = new FlxSprite();
+		lane3.makeGraphic(Std.int(FlxG.width / 8), Std.int((FlxG.height / 16) * 12), FlxColor.fromString("#ff3e3e3e"));
+		lane3.screenCenter(X);
+		lane3.y = Std.int(FlxG.height / 16);
+		lane3.x += Std.int(lane3.width / 2) * 1;
+		add(lane3);
+		lane4 = new FlxSprite();
+		lane4.makeGraphic(Std.int(FlxG.width / 8), Std.int((FlxG.height / 16) * 12), FlxColor.fromString("#ff3e3e3e"));
+		lane4.screenCenter(X);
+		lane4.y = Std.int(FlxG.height / 16);
+		lane4.x += Std.int(lane4.width / 2) * 2;
+		add(lane4);
+
+		note1 = new FlxSprite(0, 0, Assets.ClassicNote__png);
 	}
 
 	public override function update(dt:Float) {
