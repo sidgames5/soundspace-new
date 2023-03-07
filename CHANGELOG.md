8b98453a1b1a3c2d1f1afae38570c74ddd503aa4
BasicCorruption <saytleader@outlook.com>
2023-03-07 16:27:55 -0500

    Finalize 1.0.0-nightly.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.0.0-nightly.2
+
+## Bugs Fixed
+
+[#1 - Error when pressing play button](https://github.com/BasicCorruption/soundspace-new/issues/1)
--- src/main/haxe/com/neonblade/soundspace/Reference.hx
+++ src/main/haxe/com/neonblade/soundspace/Reference.hx
@@ -3,7 +3,7 @@ package com.neonblade.soundspace;
 class Reference {
 	public static final APP_NAME:String = "SoundSpace";
 	public static final APP_ID:String = "soundspace";
-	public static final VERSION = "1.0.0-nightly.1";
+	public static final VERSION = "1.0.0-nightly.2";
 
 	public static var VOLUME_MULTIPLIER:Float = 1;
 }
